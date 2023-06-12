646116eeb89f681617f6b20f16b755fcbe4b5645
brentkulwicki <brentkulwicki@gmail.com>
2023-06-12 15:22:02 -0400

    updated connection and migrations

--- main.go
+++ main.go
@@ -1,41 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
-//type user struct {
-//	id        int
-//	name      string
-//	email     string
-//	createdAt string
-//}
-
-//func main() {
-//	r := gin.Default()
-//	r.POST("/users", postUser)
-//
-//	r.run("localhost:8085")
-//}
-//
-//func postUser(c *gin.Context) {
-//	var newUser user
-//
-//	if err := c.BindJSON(&newUser); err != nil {
-//		return
-//	}
-//
-//	c.IndentedJSON(http.StatusCreated, newUser)
-//}
+const (
+	host     = "localhost"
+	port     = 5433
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "fitness_app"
+)
 
 func main() {
+	pgInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", pgInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 	r.POST("/users", func(c *gin.Context) {
 
 		page := c.DefaultQuery("page", "0")
 		name := c.PostForm("name")
 		email := c.PostForm("email")
+		sqlStatement := `INSERT INTO users (name, email, created_at)
+		VALUES ($1, $2, now())`
+
+		_, err := db.Exec(sqlStatement, name, email)
+		if err != nil {
+			panic(err)
+		}
 
 		fmt.Printf("page: %s; name: %s; message: %s", page, name, email)
 	})
